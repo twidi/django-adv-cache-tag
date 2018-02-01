fb800bf48320897269bec3b587d2c8b30d3ad5bc
Stephane Angel (Twidi) <s.angel@twidi.com>
2018-02-01 21:06:11 +0100

    Manage removal of TEMPLATE_DEBUG in recent django

--- adv_cache_tag/tests/testproject/settings.py
+++ adv_cache_tag/tests/testproject/settings.py
@@ -36,3 +36,13 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': False
+        }
+    },
+]
